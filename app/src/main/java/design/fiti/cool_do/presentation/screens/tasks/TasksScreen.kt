b65a4d886028ae13a971745ce96401a954856da5
Igadwa Magomere <83275851+Brian-Magomere-Igadwa@users.noreply.github.com>
2024-05-24 10:01:49 +0300

    Implement Welcome Screen.

--- app/src/main/java/design/fiti/cool_do/presentation/screens/tasks/TasksScreen.kt
+++ app/src/main/java/design/fiti/cool_do/presentation/screens/tasks/TasksScreen.kt
@@ -1,28 +1,68 @@
 package design.fiti.cool_do.presentation.screens.tasks
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import design.fiti.cool_do.R
 import design.fiti.cool_do.presentation.navigation.AppRoutes
 
+@Preview(showSystemUi = true)
 @Composable
-fun TasksScreen(navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = stringResource(id = R.string.welcome_taskScreen))
-        Button(onClick = { navController.navigate(AppRoutes.Boards.name) }) {
-            Text(text = stringResource(id = R.string.go_to_boardsScreen))
+fun TasksScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            TaskTopBar()
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            
+
         }
+    }
+}
+
+@Composable
+private fun TaskTopBar(modifier: Modifier = Modifier) {
+    Row(horizontalArrangement = Arrangement.Start, modifier = modifier.fillMaxWidth(0.8f)) {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowLeft,
+                contentDescription = stringResource(id = R.string.up)
+            )
+        }
+        Spacer(modifier = Modifier.fillMaxWidth())
+        Text(
+            text = stringResource(id = R.string.tasks),
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
